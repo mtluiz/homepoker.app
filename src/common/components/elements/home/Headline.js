1155f96cd0e125d75aa00dd4161e739e55409785
mt <matheusluizn@gmail.com>
2022-09-11 13:32:50 -0300

    feat: welcome component

---
+++ src/common/components/elements/home/Headline.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Headline() {
+  return (
+    <div className="max-w-xl mx-auto text-center">
+      <h1 className="text-3xl font-bold sm:text-5xl logo">
+        homepoker.app
+      </h1>
+
+      <p className="mt-4 sm:leading-relaxed sm:text-xl">
+        Agile poker em qualquer lugar!
+      </p>
+
+      <div className="flex flex-wrap justify-center mt-8 gap-4">
+        <a className="block w-full px-12 py-3 text-lg font-medium text-white bg-blue-600 rounded shadow sm:w-auto active:bg-blue-500 transition-all hover:bg-blue-700 focus:outline-none focus:ring" href="/get-started">
+          Criar sala
+        </a>
+
+        <a className="block w-full px-12 py-3 text-lg font-medium text-blue-600 rounded shadow sm:w-auto hover:text-blue-400 transition-all active:text-blue-500 focus:outline-none focus:ring" href="/about">
+          Juntar-me a uma sala
+        </a>
+      </div>
+    </div>
+  )
+}
