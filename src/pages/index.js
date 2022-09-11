14a4ffd05086726a02519a3a0315998636ab9110
mt <matheusluizn@gmail.com>
2022-09-11 13:32:08 -0300

    chore: move js to src folder

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Home from "../common/components/elements/home"
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Home />
+    </div>
+  )
+}
