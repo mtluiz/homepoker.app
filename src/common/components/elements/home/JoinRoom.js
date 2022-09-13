1633da22c9b0bfbca1cb09ebdb2c755af373878d
mt <matheusluizn@gmail.com>
2022-09-13 07:12:17 -0300

    feat: create join room input logic

--- src/common/components/elements/home/JoinRoom.js
+++ src/common/components/elements/home/JoinRoom.js
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, {useRef, useEffect} from 'react'
 
 export default function JoinRoom() {
+  
+  const fields = useRef(null);
+
+  useEffect(() => {
+    const inputs = fields.current.querySelectorAll("input");
+    Array.from(inputs).forEach((input) => {
+      input.addEventListener('keyup', (el) => {
+        if(el.target.value.length === 1 && el.target.nextElementSibling){
+          el.target.nextElementSibling.focus()
+        }
+        if((el.key == "Backspace" || el.key == "Delete") && el.target.previousElementSibling){
+          el.target.value = ""
+          el.target.previousElementSibling.focus()
+        }
+      })
+    })
+  }, [])
+  
   return (
-    <div>JoinRoom</div>
+    <div>
+      <h1 className='text-center text-2xl my-8'>Digite o codigo da sala:</h1>
+      <div>
+        <form action="">
+          <fieldset ref={fields} className='codefield'>
+            <input autoComplete="off" type="text" inputMode="numeric" pattern="\d*" maxLength="1" defaultValue="" />
+            <input autoComplete="off" type="text" inputMode="numeric" pattern="\d*" maxLength="1" defaultValue="" />
+            <input autoComplete="off" type="text" inputMode="numeric" pattern="\d*" maxLength="1" defaultValue="" />
+            <input autoComplete="off" type="text" inputMode="numeric" pattern="\d*" maxLength="1" defaultValue="" />
+            <input autoComplete="off" type="text" inputMode="numeric" pattern="\d*" maxLength="1" defaultValue="" />
+            <input autoComplete="off" type="text" inputMode="numeric" pattern="\d*" maxLength="1" defaultValue="" />
+          </fieldset>
+        </form>
+      </div>
+    </div >
   )
 }
