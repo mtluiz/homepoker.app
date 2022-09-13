1905a9efdf29ae1237773af24b715b3951a32f01
mt <matheusluizn@gmail.com>
2022-09-13 07:12:40 -0300

    chore: log api call

--- src/common/components/elements/home/index.js
+++ src/common/components/elements/home/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import CreateRoom from './CreateRoom';
+import JoinRoom from './JoinRoom';
 
 export default function Home() {
   return (
       <div className="px-4 py-32 mx-auto min-w-fit max-w-screen-xl lg:h-screen lg:items-center lg:flex">
-        <CreateRoom></CreateRoom>
+        <JoinRoom></JoinRoom>
       </div>
   )
 }
