ba6e9c43b4edd8676bd926027bdc03b40923eb27
mt <matheusluizn@gmail.com>
2022-09-18 15:50:06 -0300

    feat: create home component

--- src/common/components/elements/home/index.js
+++ src/common/components/elements/home/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import useSelectedStore from '../../../store/selected';
 import CreateRoom from './CreateRoom';
 import JoinRoom from './JoinRoom';
+import Welcome from "./Welcome";
+
+const ScreenOptions = {
+  "create": (<CreateRoom />),
+  "join": (<JoinRoom />),
+  "welcome": (<Welcome />)
+}
 
 export default function Home() {
+  const selected = useSelectedStore((state) => state.selected)
+  console.log(selected);
   return (
       <div className="px-4 py-32 mx-auto min-w-fit max-w-screen-xl lg:h-screen lg:items-center lg:flex">
-        <JoinRoom></JoinRoom>
+        {ScreenOptions[selected] || <Welcome />}
       </div>
   )
 }
