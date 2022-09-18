4ff0ad49d684390afc626a888a768ca8b58c9467
mt <matheusluizn@gmail.com>
2022-09-18 15:50:26 -0300

    feat: add toast container to app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,12 @@
+import { ToastContainer } from "react-toastify"
 import "../styles/global.css";
+import 'react-toastify/dist/ReactToastify.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => (
+  <>
+    <Component {...pageProps} />
+    <ToastContainer />
+  </>
+)
 
 export default MyApp
