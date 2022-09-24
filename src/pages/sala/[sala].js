3c636121fff20003c9da7e75683a461253e2c9f5
mt <matheusluizn@gmail.com>
2022-09-24 18:02:46 -0300

    feat: grab room object from socket

--- src/pages/sala/[sala].js
+++ src/pages/sala/[sala].js
@@ -1,20 +1,27 @@
+import io from "socket.io-client";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react"
-import { getName } from "../../utils/username";
 import { toast } from "react-toastify";
-import io from "socket.io-client";
+
 import Room from "../../common/components/elements/room";
+import { getName } from "../../utils/username";
 
 export default function Sala() {
 
   const router = useRouter()
+
+  const [roomInfo, setRoomInfo] = useState(null)
+
+  useEffect(() => {
+    console.log(roomInfo);
+  }, [roomInfo])
 
   useEffect(() => {
     console.log("quantas vez rodo");
     if(!router.isReady) return;
     const name = getName()
     const room = router.query.sala
-    //socketInitializer(name, room)
+    socketInitializer(name, room)
   }, [router.isReady])
 
   const socketInitializer = async (name, room) => {
@@ -32,6 +39,8 @@ export default function Sala() {
     socket.on('user-connected', (valor) => {
       console.log("usuario conectado", valor);
       toast.info(`O usuario ${valor.username} entrou na sala ${valor.room}`, {autoClose: 3000})
+      setRoomInfo(valor.roomObject)
+      console.log(valor.roomObject);
     })
 
     socket.on('user-disconnected', (valor) => {
@@ -42,7 +51,7 @@ export default function Sala() {
 
   return (
     <div className="min-w-fit h-screen p-8 bg-gradient-to-l from-blue-600 to-blue-900">
-      <Room roomId={router.query.sala} />
+      {roomInfo? <Room roomId={router.query.sala} roomSocketInfo={roomInfo} /> : "Loading"}
     </div>
   )
 }
