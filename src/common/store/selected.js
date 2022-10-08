9ed375798cd73baeb80875332303e962c86f8fa4
mt <matheusluizn@gmail.com>
2022-10-08 06:35:57 -0300

    chore: removing unused state

--- src/common/store/selected.js
+++ src/common/store/selected.js
@@ -2,10 +2,10 @@ import create from "zustand";
 
 const useSelectedStore = create((set) => ({
   selected: "default",
-  selectCreate: () => set((state) => ({selected: "create"})),
-  selectWelcome: () => set((state) => ({selected: "welcome"})),
-  selectJoin: () => set((state) => ({selected: "join"})),
-  selectDefault: () => set((state) => ({selected: "default"})),
+  selectCreate: () => set(() => ({selected: "create"})),
+  selectWelcome: () => set(() => ({selected: "welcome"})),
+  selectJoin: () => set(() => ({selected: "join"})),
+  selectDefault: () => set(() => ({selected: "default"})),
 }))
 
 export default useSelectedStore;
