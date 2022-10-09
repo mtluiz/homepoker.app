15db24a4e7c475fb2282a6965c9ed73e8903f7a5
mt <matheusluizn@gmail.com>
2022-10-08 21:03:15 -0300

    feat: create click events and making cards smaller

--- src/common/components/elements/room/VoteCards.js
+++ src/common/components/elements/room/VoteCards.js
@@ -33,7 +33,7 @@ export default function VoteCards({ type, hasVoted, vote, clickOnCard, selected 
   const [voted, setVoted] = useState(null);
 
   return (
-    <div className="votes h-1/4 min-h-1/4 cursor-pointer px-10 gap-4 shadow-xl overflow-x-scroll flex items-center absolute w-full bg-white bottom-0 left-0">
+    <div className="votes h-1/4 cursor-pointer p-10 gap-4 shadow-xl overflow-x-scroll overflow-y-hidden flex items-center absolute w-full bg-white bottom-0 left-0">
       <Card value={<Image width={80} height={80} src={"/images/thumbs-up.svg"} onClick={() => {clickOnCard("thumbs-up");}} alt="" />} />
       <Card value={<Image width={80} height={80} src={"/images/thumbs-down.svg"} onClick={() => {clickOnCard("thumbs-down");}} alt="" />} />
       {
