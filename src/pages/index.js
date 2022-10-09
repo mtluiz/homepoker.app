bed6123d2798ab7b9409fa3e7a8820add17a8764
mt <matheusluizn@gmail.com>
2022-10-09 00:48:03 -0300

    feat: add favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,20 @@
+import Head from "next/head"
+
 import Home from "../common/components/elements/home"
 
 export default function App() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <Home />
-    </div>
+    <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <title>Homepoker.app - Agile Poker Online</title>
+      </Head>
+      <div className="flex items-center justify-center h-screen">
+        <Home />
+      </div>
+    </>
   )
 }
