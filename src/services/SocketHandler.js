86a804801fd0bb654c6ca62d0f8c9f1d66897b0d
mt <matheusluizn@gmail.com>
2022-10-08 22:45:37 -0300

    feat: create socket reset and visibility events

--- src/services/SocketHandler.js
+++ src/services/SocketHandler.js
@@ -1,10 +1,10 @@
-export default class SocketHandler{
-  constructor(io){
+export default class SocketHandler {
+  constructor(io) {
     this.io = io;
     this.ROOMS = new Map();
   }
 
-  #generateRoomObject({type, user, roomId}){
+  #generateRoomObject({ type, user, roomId }) {
     return {
       type: type || "fibonacci",
       users: [],
@@ -14,7 +14,7 @@ export default class SocketHandler{
     }
   }
 
-  #generateUserObject({name, id}){
+  #generateUserObject({ name, id }) {
     return {
       id,
       name,
@@ -23,7 +23,7 @@ export default class SocketHandler{
     }
   }
 
-  start(){
+  start() {
     this.io.on('connection', (socket) => {
       console.log('connection', socket.id);
 
@@ -31,49 +31,74 @@ export default class SocketHandler{
         socket.join(info.room);
         console.log("Usuario se juntou a sala:", info);
 
-        if(!this.ROOMS.get(info.room)){
+        if (!this.ROOMS.get(info.room)) {
           this.ROOMS.set(info.room, this.#generateRoomObject({
-            type: info.type, 
-            user: info.name, 
+            type: info.type,
+            user: info.name,
             roomId: info.room
           }))
-        } 
+        }
 
         const currentRoom = this.ROOMS.get(info.room)
         currentRoom.users.push(this.#generateUserObject({
-            id: socket.id,
-            name: info.name
+          id: socket.id,
+          name: info.name
         }))
 
         this.io.in(info.room).emit('user-connected', {
-          username: info.name, 
+          username: info.name,
           room: info.room,
           roomData: currentRoom,
           yourId: socket.id
         })
 
         socket.on("user-vote", data => {
-            console.log(data);
-            console.log(socket.id);
-            
-            const {vote} = data;
+          console.log(data);
+          console.log(socket.id);
 
-            currentRoom.users = currentRoom.users.map(user => {
-              if(user.id === socket.id) {
-                return {
-                  ...user,
-                  hasVoted: vote !== null,
-                  vote
-                }
+          const { vote } = data;
+
+          currentRoom.users = currentRoom.users.map(user => {
+            if (user.id === socket.id) {
+              return {
+                ...user,
+                hasVoted: vote !== null,
+                vote
               }
-              return user;
-            }) 
+            }
+            return user;
+          })
 
-            this.io.in(info.room).emit('sync-votes', {
-              username: info.name, 
-              room: info.room,
-              roomData: currentRoom
-            })
+          this.io.in(info.room).emit('sync-votes', {
+            username: info.name,
+            room: info.room,
+            roomData: currentRoom
+          })
+        })
+
+        socket.on("toggle-votes-visibility", data => {
+          currentRoom.isVotesHidden = data;
+
+          this.io.in(info.room).emit('sync-votes', {
+            username: info.name,
+            room: info.room,
+            roomData: currentRoom
+          })
+        })
+
+        socket.on("reset-room", () => {
+          currentRoom.isVotesHidden = true;
+          currentRoom.users = currentRoom.users.map(user => ({
+            ...user,
+            hasVoted: false,
+            vote: null
+          }))
+
+          this.io.in(info.room).emit('sync-votes', {
+            username: info.name,
+            room: info.room,
+            roomData: currentRoom
+          })
         })
 
         socket.on('disconnect', () => {
@@ -82,7 +107,7 @@ export default class SocketHandler{
           currentRoom.users = currentRoom.users.filter(user => socket.id !== user.id)
 
           this.io.in(info.room).emit('user-disconnected', {
-            username: info.name, 
+            username: info.name,
             room: info.room,
             roomData: currentRoom
           })
