60e3b57b344cd763bd3b66d48a20547ee936d5df
mt <matheusluizn@gmail.com>
2022-10-09 19:41:01 -0300

    chore: remove unused log

--- src/common/components/elements/home/index.js
+++ src/common/components/elements/home/index.js
@@ -12,7 +12,7 @@ const ScreenOptions = {
 
 export default function Home() {
   const selected = useSelectedStore((state) => state.selected)
-  console.log(selected);
+  
   return (
       <div className="px-4 py-32 mx-auto min-w-fit max-w-screen-xl lg:h-screen lg:items-center lg:flex">
         {ScreenOptions[selected] || <Welcome />}
