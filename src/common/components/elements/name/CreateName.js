f39bd0667cdf1bc6828648490d90a25a57994f31
mt <matheusluizn@gmail.com>
2022-10-12 12:38:23 -0300

    refactor: make name function simpler

--- src/common/components/elements/name/CreateName.js
+++ src/common/components/elements/name/CreateName.js
@@ -4,22 +4,21 @@ import { getName, saveName } from '../../../../utils/username';
 
 export default function CreateName() {
 
-  const [name, setName] = useState(getName());
-
-  const handleNameTyping = (e) => {
-    saveName(name)
+  const handleNameSave = (e) => {
+    if(e.target.value) saveName(e.target.value)
   }
 
   return (
     <>
       <h1 className='text-3xl text-center my-3 font-extralight'>Qual seu nome?</h1>
-
       <input 
         className="relative block px-3 py-2 border-2 border-gray-200 rounded-lg w-full text-lg focus:ring-0" 
         id="name" 
-        onKeyUp={handleNameTyping} 
+        onKeyUp={handleNameSave} 
+        onChange={handleNameSave}
         type="text" 
         placeholder="Jeann Mallman" 
+        defaultValue={getName() || ""}
       />
     </>
   )
