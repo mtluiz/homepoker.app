3fec77df97f233813afb4cb143197184a97c5968
mt <matheusluizn@gmail.com>
2022-10-22 12:42:17 -0300

    feat: remove log and pass room type

--- src/common/components/elements/room/index.js
+++ src/common/components/elements/room/index.js
@@ -1,6 +1,6 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
 import React, { useEffect } from 'react';
 
-import useSelectedStore from '../../../store/selected';
 import copyToClipboard from '../../../../utils/copyToClipboard';
 import UserCard from '../../ui/UserCard';
 import VoteCards from './VoteCards';
@@ -10,21 +10,15 @@ import VoteParser from './VoteParser';
 
 export default function Room({ roomId, roomSocketInfo, clickOnVoteCard, toggleVoteShow, resetRoom }) {
 
-  const selectDefault = useSelectedStore((state) => state.selectDefault);
-
-  useEffect(() => {
-    console.log(roomSocketInfo.users);
-  }, [roomSocketInfo])
-
   return (
     <div className="">
       <header className='flex justify-center'>
-        <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-flex items-center absolute top-8 left-8" onClick={selectDefault}>
+        <a href='/' className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-flex items-center absolute top-8 left-8" >
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-bar-left" viewBox="0 0 16 16">
             <path fillRule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5zM10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5z" />
           </svg>
           <span>Voltar</span>
-        </button>
+        </a>
 
         <button className="bg-white hover:bg-blue-100 text-blue-600 font-bold py-2 px-4 rounded inline-flex items-center justify-center text-1xl" onClick={() => { copyToClipboard(roomId) }}>
           <span>Sala: {roomId}</span>
@@ -69,7 +63,7 @@ export default function Room({ roomId, roomSocketInfo, clickOnVoteCard, toggleVo
       </div>
 
 
-      <VoteCards clickOnCard={clickOnVoteCard} />
+      <VoteCards clickOnCard={clickOnVoteCard} type={roomSocketInfo.type} />
     </div>
   )
 }
