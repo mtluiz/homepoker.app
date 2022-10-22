11c16059289568fb4b4089e25cd488798b8bfe39
mt <matheusluizn@gmail.com>
2022-10-22 12:43:15 -0300

    feat: create name page

---
+++ src/pages/name/index.js
@@ -0,0 +1,21 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
+import React from 'react'
+import { useRouter } from 'next/router'
+import dynamic from 'next/dynamic'
+const CreateName = dynamic(() => import('../../common/components/elements/name/CreateName'), { ssr: false })
+
+export default function Name() {
+
+  const router = useRouter()
+
+  return (
+    <div className='w-full h-[100vh] flex items-center justify-center'>
+      <div>
+        <CreateName />
+        <a href={`/sala/${router.query.sala}`} className="block mt-4 w-full px-12 py-3 text-lg font-medium text-blue-600 rounded shadow sm:w-auto hover:text-blue-400 transition-all active:text-blue-500 focus:outline-none focus:ring cursor-pointer">
+          Juntar-me a sala {router.query.sala}
+        </a>
+      </div>
+    </div>
+  )
+}
